58e44d8c0c7e2241d45d8aaccd3dba8317d2c392
ernado <ernado@ya.ru>
2013-11-27 03:01:08 +0400

    bump

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,7 @@ sudo apt-get update
 sudo apt-get install python-pip python-dev redis-server mysql-server apache2 memcached libmysqlclient-dev nginx -y
 easy_install -U distribute
 pip install virtualenv
-pip install -r requirements.txt
+virtualenv env
+env/bin/python pip install -r requirements.txt
+
 
