2fdffacf9f7317509d1ac99c4a6a4addf0724baf
ernado <ernado@ya.ru>
2013-11-27 02:57:58 +0400

    bump

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+echo 'Deploying faq'
 sudo apt-get update
 sudo apt-get install python-pip python-dev redis-server mysql-server apache2 memcached libmysqlclient-dev nginx -y
 easy_install -U distribute
