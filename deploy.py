0e6205f8950b8228bf4510e43a4cb4be9a644a76
ernado <ernado@ya.ru>
2013-11-29 19:32:45 +0400

    deployment script update

--- deploy.py
+++ deploy.py
@@ -2,7 +2,7 @@ import MySQLdb as db
 
 if __name__ == '__main__':
     print 'Deploying project'
-    con = db.connect(username="root")
+    con = db.connect(user="root")
     cur = con.cursor()
     print 'Creating database'
     cur.execute('CREATE DATABASE faq_test;')
