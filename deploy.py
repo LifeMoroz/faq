3c16ed9d056f70e403e33f070ab2ad8357a796d2
ernado <ernado@ya.ru>
2013-11-29 20:03:21 +0400

    updated

--- deploy.py
+++ deploy.py
@@ -2,7 +2,7 @@ import MySQLdb as db
 
 if __name__ == '__main__':
     print 'Deploying project'
-    con = db.connect(user="root")
+    con = db.connect(user="root", passwd="local")
     cur = con.cursor()
     print 'Creating database'
     cur.execute('CREATE DATABASE faq_test;')
--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 echo 'Deploying faq'
 # sudo apt-get update
-sudo debconf-set-selections <<< 'mysql-server-<version> mysql-server/root_password password ""'
-sudo debconf-set-selections <<< 'mysql-server-<version> mysql-server/root_password_again password ""'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password local'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password local'
 sudo apt-get install python-pip python-dev mysql-server apache2 libmysqlclient-dev nginx -y
 easy_install -U distribute
-pip install virtualenv
+
+echo 'Downloading and installing redis'
+wget http://download.redis.io/redis-stable.tar.gz
+tar xvzf redis-stable.tar.gz
+cd redis-stable 
+make
+sudo ln src/ -s
+cd -
+
+sudo pip install virtualenv
 virtualenv env
 env/bin/pip install -r requirements.txt
-env/bin/python deploy.py
+# env/bin/python deploy.py
 
 
