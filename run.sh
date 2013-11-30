1ba40146253cf1482529ceb8dd6fc278567bf893
ernado <ernado@ya.ru>
2013-11-30 21:00:21 +0400

    added nginx config update in runscript

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo 'Starting all services'
 ./manage.py make
+
+echo 'Need sudo to copy nginx config'
+sudo cp conf/faq-nginx.conf /etc/nginx/sites-enabled/faq.conf
+sudo service nginx reload
+echo 'Ok'
+
 ./manage.py realtime & > logs/realtime-out.log 2> logs/realtime-err.log
 uwsgi --ini conf/faq-uwsgi.ini
 searchd -c conf/faq-sphinx.conf
